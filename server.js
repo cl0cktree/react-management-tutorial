6c39ace8347948a348589220538e2cb81df8dcf8
Lee.Hyunsup <clocktree@live.co.kr>
2021-03-05 18:45:13 +0900

    20210305_2

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,41 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers',(req, res)=>{
-  res.send([
-    {
-      'id' : 1,
-      'image' : 'https://placeimg.com/64/64/1',
-      'name': '이름01',
-      'birthday' : '821111',
-      'gender' : '남자',
-      'job' : '대학생1'
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name': '이름02',
-    'birthday' : '821112',
-    'gender' : '남자',
-    'job' : '대학생2'
-  },
-  {
-    'id' : 3,
-    'name': '이름03',
-    'image' : 'https://placeimg.com/64/64/3',
-    'birthday' : '821113',
-    'gender' : '남자',
-    'job' : '대학생3'
-  },
-  {
-    'id' : 4,
-    'name': '이름04',
-    'image' : 'https://placeimg.com/64/64/4',
-    'birthday' : '821114',
-    'gender' : '남자',
-    'job' : '대학생4'
-  }
-  ]);
+  connection.query(
+    "SELECT * FROM management.CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
